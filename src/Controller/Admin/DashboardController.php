9f423173ff379c72334be9739122ddb32b13cbb0
mlejeune <mlejeunepro@gmail.com>
2024-03-19 19:09:27 +0100

    feat: add Crud to admin User

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Controller\Admin\Users\UserCrudController;
+use App\Entity\User\AbstractUser;
+use App\Entity\User\UserAdministrator;
+use App\Entity\User\UserClient;
+use App\Repository\User\AbstractUserRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMINISTRATOR')]
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @param AbstractUserRepository $userRepository
+     */
+    public function __construct(private readonly AbstractUserRepository $userRepository)
+    {}
+
+    /**
+     * @return Response
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
+        return $this->redirect(
+            $adminUrlGenerator->setController(UserCrudController::class)
+                ->generateUrl()
+        );
+    }
+
+    /**
+     * @return Dashboard
+     */
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('CarsFleet')
+            ->setTranslationDomain('admin')
+            ->disableUrlSignatures();
+    }
+
+    /**
+     * @return iterable
+     */
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::subMenu('Utilisateurs', 'fas fa-users')->setSubItems([
+            MenuItem::linkToCrud('Tous', null, AbstractUser::class),
+            MenuItem::linkToCrud('Administrateurs', null, UserAdministrator::class),
+            MenuItem::linkToCrud('Clients', null, UserClient::class),
+        ])->setBadge($this->userRepository->count([]), 'primary');
+
+        yield MenuItem::section('Paramètres du compte');
+        yield MenuItem::linkToLogout('Déconnexion', 'fa fa-sign-out');
+    }
+
+    /**
+     * @param UserInterface $user
+     * @return UserMenu
+     */
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        return parent::configureUserMenu($user)
+            ->setName(sprintf('%s %s | %s',
+                $user->getFirstName(),
+                $user->getLastName(),
+                $user->getEmail()
+            ));
+    }
+}
---
+++ src/Controller/Admin/Users/UserCrudController.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin\Users;
+
+use App\Entity\User\AbstractUser;
+use App\Enum\Role;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMINISTRATOR')]
+class UserCrudController extends AbstractCrudController
+{
+    /**
+     * @return string
+     */
+    public static function getEntityFqcn(): string
+    {
+        return AbstractUser::class;
+    }
+
+    /**
+     * @param Crud $crud
+     *
+     * @return Crud
+     */
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Utilisateur')
+            ->setEntityLabelInPlural('Utilisateurs');
+    }
+
+    /**
+     * @param Actions $actions
+     *
+     * @return Actions
+     */
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->remove(Crud::PAGE_INDEX, Action::NEW);
+    }
+
+    /**
+     * @param Filters $filters
+     *
+     * @return Filters
+     */
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(ChoiceFilter::new('roles')->setChoices(Role::asArrayInverted()));
+    }
+
+    /**
+     * @param string $pageName
+     *
+     * @return iterable
+     */
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            EmailField::new('email', 'Email'),
+            TextField::new('firstName', 'Prénom'),
+            TextField::new('lastName', 'Nom'),
+            ChoiceField::new('roles', 'Roles')
+                ->allowMultipleChoices()
+                ->setChoices(Role::asArrayInverted()),
+        ];
+    }
+}
