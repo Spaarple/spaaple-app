b33fc2f2d665df489fd26def64b40fa873463f27
mlejeune <mlejeunepro@gmail.com>
2024-03-20 22:16:53 +0100

    feat/ make header/footer

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,10 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
+// Scroll event listener to add shadow to header
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    window.scrollY > 0 ? header.classList.add('shadow-md') : header.classList.remove('shadow-md');
+});
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
