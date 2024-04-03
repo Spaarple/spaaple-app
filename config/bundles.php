2d9eb21761a2e062717cd92c440636f73c584171
mlejeune <mlejeunepro@gmail.com>
2024-04-03 20:47:04 +0200

    fix/ horo

--- config/bundles.php
+++ config/bundles.php
@@ -18,5 +18,4 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
