0c08516c9176116a638f1884bafb4cfb75a4b732
mlejeune <mlejeunepro@gmail.com>
2024-04-08 21:26:41 +0200

    feat: Bulk message

--- src/Controller/Admin/BulkContactCrudController.php
+++ src/Controller/Admin/BulkContactCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\BulkContact;
+use App\Service\AlertServiceInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -10,9 +12,27 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 
 class BulkContactCrudController extends AbstractCrudController
 {
+    /**
+     * @param AlertServiceInterface $alertService
+     * @param MailerInterface $mailer
+     * @param ParameterBagInterface $parameterBag
+     */
+    public function __construct(
+        private readonly AlertServiceInterface $alertService,
+        private readonly MailerInterface $mailer,
+        private readonly ParameterBagInterface $parameterBag,
+    )
+    {
+    }
+
     /**
      * @return string
      */
@@ -41,7 +61,9 @@ class BulkContactCrudController extends AbstractCrudController
     public function configureActions(Actions $actions): Actions
     {
         return $actions
-            ->add(Crud::PAGE_INDEX, Action::DETAIL);
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+            ->remove(Crud::PAGE_DETAIL, Action::EDIT);
     }
 
     /**
@@ -51,9 +73,39 @@ class BulkContactCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            ArrayField::new('email'),
-            TextField::new('subject'),
-            TextEditorField::new('message'),
+            ArrayField::new('email', 'Destinataires'),
+            TextField::new('subject', 'Sujet'),
+            TextEditorField::new('message', 'Message Commun')
+                ->setNumOfRows(20),
         ];
     }
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param $entityInstance
+     * @return void
+     * @throws TransportExceptionInterface
+     */
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        foreach ($entityInstance->getEmail() as $email) {
+            $email = (new TemplatedEmail())
+                ->from(new Address($this->parameterBag->get('mail.support'), 'Spaarple'))
+                ->to($email)
+                ->subject($entityInstance->getSubject())
+                ->textTemplate('admin/email/bulk_contact.txt.twig')
+                ->htmlTemplate('admin/email/bulk_contact.html.twig')
+                ->context([
+                    'message' => $entityInstance->getMessage(),
+                ]);
+
+            $this->mailer->send($email);
+        }
+
+        $this->alertService->success('Message envoyé avec succès !');
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+
 }
