5859686184417592bc10469c806e99ec3b0e17ad
mlejeune <mlejeunepro@gmail.com>
2024-04-17 21:35:38 +0200

    fix: pipeline

--- src/Entity/User/UserClient.php
+++ src/Entity/User/UserClient.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace App\Entity\User;
 
-use App\Entity\Estimate;
 use App\Enum\Role;
 use App\Repository\User\UserClientRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserClientRepository::class)]
